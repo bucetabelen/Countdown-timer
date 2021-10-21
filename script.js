9924df1bcf9ff9d3448512e9871b47a21a17d34a
bucetabelen <bbuceta@fi.uba.ar>
2021-10-21 12:57:37 -0300

    agregando funcionalidad para personalizar la fecha del countdown timer

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function updateTimer() {
-    future = Date.parse("jan 01, 2022 00:00:00");
+
+function getInputValue(){
+    // Selecting the input element and get its value 
+    const inputVal = document.getElementById("input").value;
+    console.log(inputVal);
+    //updateTimer((inputVal));
+ 
+    setInterval(function(){ updateTimer(inputVal); }, 1000);
+    
+    //sconsole.log(new Date(inputVal));
+    //console.log(Date.parse(new Date(inputVal)));
+    
+}   
+
+
+
+
+
+function updateTimer(value) {
+
+    //future = Date.parse(value);
+    future = new Date(value.replace(/-/g, '\/'));
+    console.log("future: ",future);
+    //f = Date.parse(future);
+    //future = Date.parse("oct 22, 2021 00:00:00");
     now = new Date();
+    console.log("now ",now);
     diff = future - now;
+    console.log("diff ", diff);
 
     days = Math.floor(diff / (1000 * 60 * 60 * 24));
     hours = Math.floor(diff / (1000 * 60 * 60));
@@ -13,6 +38,7 @@ function updateTimer() {
     m = (mins - hours * 60)<10 ? "0"+(mins - hours * 60):(mins - hours * 60);
     s = (secs - mins * 60)<10 ? "0"+(secs - mins * 60):(secs - mins * 60);
 
+    //console.log(d);
     document.getElementById("timer")
     .innerHTML =
 
@@ -31,7 +57,10 @@ function updateTimer() {
     '<div>'+'<span>Hours</span></div>'+
     '<div>'+'<span>Minutes</span></div>'+
     '<div>'+'<span>Seconds</span></div>';
-   
+  
+    document.getElementById("input").style.display='none';
+    document.getElementById("btn").style.display='none';
+
    
 }
-setInterval('updateTimer()', 1000);
+//setInterval('updateTimer()', 1000);
