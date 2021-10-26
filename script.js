279bc8f6e34604ed25e2f46da3cafb8d46ba8f31
bucetabelen <bbuceta@fi.uba.ar>
2021-10-25 21:26:07 -0300

    Prevencion y manejo de errores para input null o invalido + aplicando estilo al input tag date

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
+const ERROR_MESSAGE_INPUT_IS_NULL = "Ups! Tenes que elegir una fecha"
+const ERROR_MESSAGE_INPUT_IS_INVALID_DATE = "Alto ahi, esa fecha ya pasó u.u"
+
+
+function handleError(errorMessage){
+
+    window.alert(errorMessage);
+    window.location.reload(true);
+    return false;
+}
+function handleErrorInputIsNull(){
+
+    handleError(ERROR_MESSAGE_INPUT_IS_NULL);
+}
+function handleErrorInputIsInvalid(){
+
+    handleError(ERROR_MESSAGE_INPUT_IS_INVALID_DATE);
+}
+
+function preventErrors(value){
+
+    future = new Date(value.replace(/-/g, '\/'));
+    now = new Date();
+    if( (future.getTime() < now.getTime())){
+       
+        handleErrorInputIsInvalid();
+    }
+    else if(value == ""){
+
+        handleErrorInputIsNull();
+          
+    }
+}
 
 function getInputValue(){
     // Selecting the input element and get its value 
     const inputVal = document.getElementById("input").value;
-    console.log(inputVal);
-    //updateTimer((inputVal));
+   
+    preventErrors(inputVal);
+   
  
     setInterval(function(){ updateTimer(inputVal); }, 1000);
     
-    //sconsole.log(new Date(inputVal));
-    //console.log(Date.parse(new Date(inputVal)));
     
 }   
 
 
-
-
-
 function updateTimer(value) {
 
+    
     //future = Date.parse(value);
     future = new Date(value.replace(/-/g, '\/'));
     console.log("future: ",future);
@@ -25,6 +55,7 @@ function updateTimer(value) {
     //future = Date.parse("oct 22, 2021 00:00:00");
     now = new Date();
     console.log("now ",now);
+    
     diff = future - now;
     console.log("diff ", diff);
 
